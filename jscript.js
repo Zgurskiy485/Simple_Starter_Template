fafb751d21a57c1236bfd240210171c7adb935e8
zgurskiy485 <zgurskiy@mail.ru>
2023-10-30 19:58:28 +0300

    new commit6

---
+++ jscript.js
@@ -0,0 +1,91 @@
+// типы данных оы
+Number
+String
+null
+undefined
+Boolean
+BigInt = // 2* 10^53-1
+Object
+Symbol
+
+
+// обозначения переменных
+
+let y = 6;
+
+const x = 'Николай'; // string
+let g = 5
+g = 'vsgd';
+
+function foo() {
+    var t = true // не использовать
+    let y = 6; //использовать
+}
+
+console.log(t) //использовать
+
+function ju() {
+
+}
+
+//математические операции
+let a = 5 * 5;
+/
++
+-
+*
+
+let b = 10 > 9;
+=> // больше или равно
+let b = 10 =! 9;
+// =! // не равно
+
+
+let b = (10 == '10'); //не строгое равенство // true
+let p = (10 === '10'); // строгое равенство //false
+
+//логические операции
+
+if(x < b){
+
+};
+if (p>a);
+{
+
+} else if{
+
+};
+
+switch (x) {
+    case 'val1'
+    [break]
+
+    case 'val1': // x === val1
+    case 'val2'
+}
+if (p>a);
+}else{
+
+let nub = (p > b)? 'vdvddf' : 456;
+
+
+const array = [a, b. 43, 56, true] //одномерный массив
+const array1 = [a, b. 43, 56, [vsvdg, djdjdj, xxkxkxk]] //vyjujvthysq массив
+const array = [obj = {
+kay: Value,
+kay: Value,
+}
+]
+
+// циклы
+let j = 5
+for(let  i=0;i>j; i++) {
+let count = 1;
+console.log(count)
+}
+
+const num = [1,2,3,4,5,35, 34]
+let i =  0
+while (i < num.lengh) {
+    let newNum = i * num[i]
+}
